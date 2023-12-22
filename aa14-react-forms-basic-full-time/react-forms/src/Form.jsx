6cbd1c680e386f158b1fa2dc8e4c643887858664
Ishan Chawla <ishan.kpv@gmail.com>
2023-12-22 15:06:35 -0500

    make form submit to console [no validations :( ]

--- aa14-react-forms-basic-full-time/react-forms/src/Form.jsx
+++ aa14-react-forms-basic-full-time/react-forms/src/Form.jsx
@@ -6,50 +6,95 @@ export default function Form() {
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [phoneNum, setPhoneNum] = useState('');
-  const [phoneTyp, setPhoneType] = useState('');
+  const [phoneType, setPhoneType] = useState('');
   const [staff, setStaff] = useState('');
   const [bio, setBio] = useState('');
-  const [signUp, setSignUp] = useState(false);
+  const [emailNotifs, setEmailNotifs] = useState(false);
 
-  
+  function handleChange(field) {
+    return (event) => {
+      switch (field) {
+      case "name":
+        setName(event.target.value);
+        break;
+      case "email":
+        setEmail(event.target.value);
+        break;
+      case "phoneNum":
+        setPhoneNum(event.target.value);
+        break;
+      case "phoneType":
+        setPhoneType(event.target.value);
+        break;
+      case "staff":
+        //event.target.value is either "intsturcot" or "student"
+        if (event.target.checked) {
+          setStaff(event.target.value);
+        } else {
+          setStaff(event.target.value);
+        }
+        break;
+      case "bio":
+        setBio(event.target.value);
+        break;
+      case "emailNotifs":
+        if (event.target.checked) {
+          setEmailNotifs(true);
+        } else {
+          setEmailNotifs(false);
+        }
+        break;
+      }
+
+    };
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // let
+    console.log(name, email, phoneNum, phoneType, staff, bio, emailNotifs);
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label htmlFor='name'>Name
-        <input type="text" value={name} id="name"/>
+        <input type="text" value={name} onChange={handleChange('name')} id="name"/>
       </label>
       <br />
       <label htmlFor='email'>Email
-        <input type="email" value={email} id="email"/>
+        <input type="email" value={email} onChange={handleChange('email')} id="email"/>
       </label>
       <br />
       <label htmlFor='phone-number'>Phone Number
-        <input type="tel" value={phoneNum} id="phone-number"/>
+        <input type="tel" value={phoneNum} onChange={handleChange('phoneNum')} id="phone-number"/>
       </label>
       <br />
       <label htmlFor='phone-type'>Phone Type </label>
-      <select name="phone-type" value={phoneTyp} id="phone-type">
+      <select name="phone-type" value={phoneType} onChange={handleChange('phoneType')} id="phone-type">
         <option value="">Please choose an option</option>
         <option value="home">Home</option>
         <option value="work">Work</option>
         <option value="mobile">Mobile</option>
       </select>
       <br />
-      <label htmlFor="staff">Staff:
-        <input type="radio" value="Instructor" checked={(staff === "Instructor") ? "true" : "false"} />
-        <input type="radio" value="Student" checked={(staff === "Student") ? "true" : "false"} />
+      <label htmlFor="staff">Staff?
+        <br/>
+        <input type="radio" value="Instructor" onChange={handleChange('staff')} checked={(staff === "Instructor") ? true : false} />
+        <label>Instructor</label>
+        <input type="radio" value="Student" onChange={handleChange('staff')} checked={(staff === "Student") ? true : false} />
+        <label>Student</label>
       </label>
       <br />
       <label htmlFor="bio">Bio
-        <input type="text" id="bio-field" value={bio}/>
+        <input type="text" id="bio-field" value={bio} onChange={handleChange('bio')}/>
       </label>
       <br />
-      <label htmlFor="sign-up">Email Notificaitons
-        <input type="checkbox" checked={(signUp) ? "true" : "false"}/>
+      <label htmlFor="email-notifs">Email Notificaitons
+        <input type="checkbox" onChange={handleChange('emailNotifs')} checked={(emailNotifs) ? true : false}/>
       </label>
       <br />
       <label htmlFor="submit">Submit Form
-        <button value="Submit"></button>
+        <button value="Submit">Submit Form</button>
       </label>
     </form>
   );
